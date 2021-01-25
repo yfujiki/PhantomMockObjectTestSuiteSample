59e0bbbefdec64c73d607bf8895b83cb24038500
Yuichi Fujiki <yuichi@shopback.com>
2021-01-25 16:51:26 +0800

    Remove Pods from repo.

--- Pods/Target Support Files/Pods-TestSuiteSample-TestSuiteSampleUITests/Pods-TestSuiteSample-TestSuiteSampleUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TestSuiteSample_TestSuiteSampleUITests : NSObject
-@end
-@implementation PodsDummy_Pods_TestSuiteSample_TestSuiteSampleUITests
-@end
